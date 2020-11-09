ca7a0d8c761c2000bfa1d25a7b642f717dc11b86
wagnkai_wlps <wangkai1@cethik.com>
2020-11-09 09:26:07 +0800

    添加cosine 支持

--- src/parser.c
+++ src/parser.c
@@ -1129,8 +1129,9 @@ learning_rate_policy get_policy(char *s)
     if (strcmp(s, "exp")==0) return EXP;
     if (strcmp(s, "sigmoid")==0) return SIG;
     if (strcmp(s, "steps")==0) return STEPS;
-    if (strcmp(s, "sgdr")==0) return SGDR;
-    fprintf(stderr, "Couldn't find policy %s, going with constant\n", s);
+	if (strcmp(s, "sgdr") == 0) return SGDR;
+	if (strcmp(s, "cosine") == 0) return COSINE;
+	fprintf(stderr, "Couldn't find policy %s, going with constant\n", s);
     return CONSTANT;
 }
 
